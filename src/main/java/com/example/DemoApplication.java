f4f1a4ccb1e94215768a373a09bd8fe1bf260d24
Maxim Tsoy <mtsoy1990@gmail.com>
2016-01-15 06:49:13 +0400

    create template for server websocket

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,90 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.core.MessageSendingOperations;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("http://localhost:8080/start");
+    }
+
+    @Configuration
+    @EnableWebSocketMessageBroker
+    @EnableScheduling
+    static class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+            config.enableSimpleBroker("/topic");
+        }
+
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry.addEndpoint("/socket").withSockJS();
+        }
+
+    }
+
+
+    @RestController
+    static class Controller {
+
+        @Autowired
+        MessagingService messagingService;
+
+        @SendTo("/topic")
+        public String show() {
+            return "ping";
+        }
+
+        @RequestMapping("/start")
+        public String start() {
+            messagingService.loginUser();
+            return "start";
+        }
+    }
+
+    @Configuration
+    static class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        }
+
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/")
+                    .setViewName("forward:/index.html");
+        }
+    }
+
+    @Service
+    static class MessagingService {
+        @Autowired
+        private MessageSendingOperations<String> messagingTemplate;
+
+        public void loginUser() {
+            String destination = "/topic";
+            this.messagingTemplate.convertAndSend(destination, "ping");
+        }
+    }
+}
